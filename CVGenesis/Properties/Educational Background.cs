1b1f697fff2f6a29dcf8135d3e8bb59c597ebe6f
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2023-03-18 14:16:19 +0600

    Finished educational

--- CVGenesis/Properties/Educational Background.cs
+++ CVGenesis/Properties/Educational Background.cs
@@ -16,5 +16,15 @@ namespace CVGenesis.Properties
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
